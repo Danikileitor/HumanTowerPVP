8b5cd816452dc9eb78a515d3d42ed5b7cce45319
Danikileitor <focusyi@hotmail.com>
2018-04-16 14:01:28 +0200

    Pos yasta, falta que se pueda apilar más de 1.

--- src/com/danikileitor/humantowerpvp/Main.java
+++ src/com/danikileitor/humantowerpvp/Main.java
@@ -21,6 +21,7 @@ import org.bukkit.inventory.PlayerInventory;
 import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.plugin.java.JavaPlugin;
 
+@SuppressWarnings("unused")
 public class Main extends JavaPlugin implements Listener{
 
 	@Override
